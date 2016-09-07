599d1c491c8d91e518e331c447ef3a90bfda954d
Thanh Nguyen <thanh@symphonycreativesolutions.com>
2016-09-07 15:38:13 +0700

    update auto layout

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Thanh Nguyen on 9/7/16.
+//  Copyright © 2016 Thanh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
